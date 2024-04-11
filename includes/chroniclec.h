7d3703cd3df33d0f719805ad11ac790a40f50a0a
fedi-nabli <fedinabli@gmail.com>
2024-04-11 13:33:11 +0100

    Added license and description to files

--- includes/chroniclec.h
+++ includes/chroniclec.h
@@ -1,3 +1,15 @@
+/**
+ * @file chroniclec.h
+ * @author Fedi Nabli (fedinabli@gmail.com)
+ * @version 1.0
+ * @date 2024-04-11
+ * 
+ * @copyright Copyright (c) 2024 Fedi Nabli
+ * 
+ * @brief This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See `chroniclec.c` or `LICENSE` for details.
+ */
+
 #ifndef CHRONICLEC_H
 #define CHRONICLEC_H
 
